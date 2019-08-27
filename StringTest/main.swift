e1f738bf9e09da64a12569951e7d223b2030d75c
Ulysses Olsen <ulysses.olsen@icloud.com>
2019-08-27 09:11:01 -0400

    Initial Commit

---
+++ StringTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StringTest
+//
+//  Created by Ulysses Olsen on 8/27/19.
+//  Copyright © 2019 Ulysses Olsen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
